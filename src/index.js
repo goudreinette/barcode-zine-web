7b23b54fb26f6cf77bd90bdf4d75529ffef2ff5a
Rein van der Woerd <reinvanderwoerd@me.com>
2020-06-21 16:38:39 +0200

    Synths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /**
  * Init
  */
-Quagga.init(
-  {
+Quagga.init({
     inputStream: {
       type: "LiveStream",
       constraints: {
@@ -43,17 +42,10 @@ Quagga.onProcessed(result => {
 
   if (result) {
     if (result.boxes) {
-      drawingCtx.clearRect(
-        0,
-        0,
-        parseInt(drawingCanvas.getAttribute("width")),
-        parseInt(drawingCanvas.getAttribute("height"))
-      );
+      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
       result.boxes
-        .filter(function(box) {
-          return box !== result.box;
-        })
-        .forEach(function(box) {
+        .filter(box => box !== result.box)
+        .forEach(box => {
           Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
             color: "black",
             lineWidth: 2
@@ -81,13 +73,27 @@ Quagga.onProcessed(result => {
 /**
  * Synth setup
  */
-// Tone, global to interact with it in console
-// const reverb = new Tone.Reverb().toMaster();
-const synth = (window.synth = new Tone.AMSynth());
+const reverb = new Tone.Reverb().toMaster();
+const distortion = new Tone.Distortion().connect(reverb);
+const phaser = new Tone.Phaser().connect(distortion);
+const bitcrusher = new Tone.BitCrusher().connect(phaser);
+const delay = new Tone.FeedbackDelay().connect(bitcrusher);
+const synth = new Tone.AMSynth().connect(delay);
 
-synth.toMaster();
-// synth.connect(reverb);
+// Make the synth dry at first -- 
+function makeDry() {
+  distortion.wet = 0
+  phaser.wet = 0
+  bitcrusher.wet = 0
+  delay.wet = 0
+}
 
+makeDry()
+
+
+/**
+ * On detected
+ */
 Quagga.onDetected(result => {
   var code = result.codeResult.code;
   executeCommand(code);
@@ -95,40 +101,43 @@ Quagga.onDetected(result => {
 
 
 function executeCommand(code) {
-  const [command, param] = code.split("-");
+  const [command, param] = code.toUpperCase().split("-");
   console.log(code, command, param);
 
   // Scanned an oscillator
   if (command == "OSC") {
+    if (param == "SINE") 
+      synth.oscillator = new Tone.Oscillator(440, "sine")
+    if (param == "SQUARE") 
+      synth.oscillator = new Tone.Oscillator(440, "square")
+    if (param == "TRIANGLE") 
+      synth.oscillator = new Tone.Oscillator(440, "triangle")
+    if (param == "SAW") 
+      synth.oscillator = new Tone.Oscillator(440, "sawtooth")
   }
 
   // Scanned an effect
   else if (command == "FX") {
+    makeDry()
+    if (param == 'DISTORTION')
+      distortion.wet = .5
+    if (param == 'PHASER')
+      phaser.wet = .5
+    if (param == 'BITCRUSHER')
+      bitcrusher.wet = .5
+    if (param == 'DELAY')
+      delay.wet = .5
   }
 
   // Scanned reverb
   else if (command == "RV") {
+    reverb.decay = parseInt(param);
   }
 
   // Scanned a note
   else {
     synth.triggerAttackRelease(code.replace("S", "#"), "16n");
   }
-
-  // NOTE
-  // if (command == "N") {
-  //   synth.triggerAttackRelease(param, "16n");
-  // }
-
-  // // FREQUENCY
-  // if (command == "F") {
-  //   synth.triggerAttackRelease(param, "16n");
-  // }
-
-  // // REVERB
-  // if (command == "R") {
-  //   reverb.decay = parseInt(command);
-  // }
 }
 
 
